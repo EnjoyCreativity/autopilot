d6a9e8c9a518d5545215a5dbe49b596ace29d0bf
David Needham <david@enjoycreativity.com>
2016-07-20 12:08:49 -0500

    Adding identification for Wordpress vs Drupal

--- autopilot.sh
+++ autopilot.sh
@@ -19,57 +19,73 @@ echo '=================================='
 echo 'AUTHENTICATE WITH PANTHEON:'
 $terminus auth login $2
 echo '=================================='
-echo 'CHECKING FOR UPDATES ON LIVE:'
-updatestatus="$($terminus drush "upc --security-only --no-core --check-updatedb=0 -n" --site=$site --env=test | grep 'SECURITY UPDATE available')"
+echo 'CHECKING THE FRAMEWORK:'
+framework="$($terminus site info --site=$1 | grep 'Framework     | drupal')"
 
-if echo "$updatestatus"; then
-    echo '=================================='
-    echo 'BACKUP EVERY ENVIRONMENT:'
-    $terminus site backups create --site=$site --env=dev --element=all
-    $terminus site backups create --site=$site --env=test --element=all
-    $terminus site backups create --site=$site --env=live --element=all
-    echo '=================================='
-    echo 'CHANGING MODE IN DEV TO GIT:'
-    $terminus site set-connection-mode --site=$site --env=dev --mode=git
-	echo '=================================='
-	echo 'APPLYING UPSTREAM UPDATES TO DEV:'
-	$terminus site upstream-updates apply --yes --site=$site --env=dev --accept-upstream --updatedb
-	echo '=================================='
-	echo 'TESTING FOR AUTOPILOT ENVIRONMENT:'
+if echo "$framework"; then
+	$framework="DRUPAL"
+	echo $framework
 
-	if $terminus site environments --site=$site | grep autopilot; then
-		echo 'SYNCING CODE AND CONTENT TO MULTIDEV'
-		$terminus site clone-content --site=$site --from-env=live --to-env=$multidev --yes
-		$terminus site merge-from-dev --site=$site --env=$multidev
+
+	echo '=================================='
+	echo 'CHECKING FOR UPDATES ON LIVE:'
+	updatestatus="$($terminus drush "upc --security-only --no-core --check-updatedb=0 -n" --site=$site --env=test | grep 'SECURITY UPDATE available')"
+
+	if echo "$updatestatus"; then
+	    echo '=================================='
+	    echo 'BACKUP EVERY ENVIRONMENT:'
+	    $terminus site backups create --site=$site --env=dev --element=all
+	    $terminus site backups create --site=$site --env=test --element=all
+	    $terminus site backups create --site=$site --env=live --element=all
+	    echo '=================================='
+	    echo 'CHANGING MODE IN DEV TO GIT:'
+	    $terminus site set-connection-mode --site=$site --env=dev --mode=git
+		echo '=================================='
+		echo 'APPLYING UPSTREAM UPDATES TO DEV:'
+		$terminus site upstream-updates apply --yes --site=$site --env=dev --accept-upstream --updatedb
+		echo '=================================='
+		echo 'TESTING FOR AUTOPILOT ENVIRONMENT:'
+
+		if $terminus site environments --site=$site | grep autopilot; then
+			echo 'SYNCING CODE AND CONTENT TO MULTIDEV'
+			$terminus site clone-content --site=$site --from-env=live --to-env=$multidev --yes
+			$terminus site merge-from-dev --site=$site --env=$multidev
+
+		else
+			echo 'CREATING NEW ENV FOR AUTOPILOT:'
+			$terminus site create-env --site=$site --from-env=dev --to-env=$multidev
+		fi
+
+		echo '=================================='
+		echo 'CHANGING MODE IN AUTOPILOT TO SFTP:'
+		$terminus site set-connection-mode --site=$site --env=$multidev --mode=sftp
+		echo '=================================='
+		echo 'APPLYING ALL SECURITY UPDATES:'
+		$terminus drush "up --security-only --no-core -y" --site=$site --env=$multidev
+		echo '=================================='
+		echo 'COMMITTING THE CODE CHANGES:'
+		$terminus site code commit --site=$site --env=$multidev --message="Autopilot: Running security updates. $updatestatus" --yes
+		echo '=================================='
+		echo 'MERGING COMMIT INTO MASTER / DEV:'
+		$terminus site merge-to-dev --site=$site --env=$multidev
+		echo '=================================='
+		echo 'DEPLOYING UPDATES INTO TEST'
+		$terminus site deploy --site=$site --env=test --sync-content --cc --updatedb --note="Autopilot: Running security updates and any other changes staged in Dev. Please double check the code tab to ensure only the updates are in this deployment before pushing to live.  $updatestatus"
+		echo '=================================='
+		echo 'SENDING EMAIL'
+		echo -e "Updates available in the Test environment of $site. Go check it out! \n $updatestatus" | mail -s "$site security updates" $3
+		echo '=================================='
+		echo 'AUTOPILOT COMPLETE!'
+		echo '=================================='
 
 	else
-		echo 'CREATING NEW ENV FOR AUTOPILOT:'
-		$terminus site create-env --site=$site --from-env=dev --to-env=$multidev
+		echo '=================================='
+		echo 'STOPPED: NO SECURITY UPDATES.'
 	fi
 
-	echo '=================================='
-	echo 'CHANGING MODE IN AUTOPILOT TO SFTP:'
-	$terminus site set-connection-mode --site=$site --env=$multidev --mode=sftp
-	echo '=================================='
-	echo 'APPLYING ALL SECURITY UPDATES:'
-	$terminus drush "up --security-only --no-core -y" --site=$site --env=$multidev
-	echo '=================================='
-	echo 'COMMITTING THE CODE CHANGES:'
-	$terminus site code commit --site=$site --env=$multidev --message="Autopilot: Running security updates. $updatestatus" --yes
-	echo '=================================='
-	echo 'MERGING COMMIT INTO MASTER / DEV:'
-	$terminus site merge-to-dev --site=$site --env=$multidev
-	echo '=================================='
-	echo 'DEPLOYING UPDATES INTO TEST'
-	$terminus site deploy --site=$site --env=test --sync-content --cc --updatedb --note="Autopilot: Running security updates and any other changes staged in Dev. Please double check the code tab to ensure only the updates are in this deployment before pushing to live.  $updatestatus"
-	echo '=================================='
-	echo 'SENDING EMAIL'
-	echo -e "Updates available in the Test environment of $site. Go check it out! \n $updatestatus" | mail -s "$site security updates" $3
-	echo '=================================='
-	echo 'AUTOPILOT COMPLETE!'
-	echo '=================================='
-
 else
-	echo '=================================='
-	echo 'STOPPED: NO SECURITY UPDATES.'
+	$framework="WORDPRESS OR OTHER"
+	echo $framework
+
+	
 fi
